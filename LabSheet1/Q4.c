2ab52c00c2fa853f484f8c41667c5beccb60efac
ArunAryal <137301055+ArunAryal@users.noreply.github.com>
2024-06-08 13:57:17 +0545

    Q4 optimized

--- LabSheet1/Q4.c
+++ LabSheet1/Q4.c
@@ -3,65 +3,118 @@ typedef struct student{
     char name[25];
     int roll;
     char address[25];
-    int phone;
+    long long phone;
 } st;
 
 #include<stdio.h>
 #include<string.h>
-# define max 50
+#include<stdlib.h>
 
-void input(int num,st *rec){
-    // printf("This is input function\n");
-    for (int i = 0; i < num; i++)
-    {
-        printf("Enter the name of student:\t");
-        scanf("%s",&rec[i].name);
-        printf("Enter the roll number:\t\t");
-        scanf("%d",&rec[i].roll);
-        printf("Enter the address:\t\t");
-        scanf("%s",&rec[i].address);
-        printf("Enter the phone number:\t\t");
-        scanf("%d",&rec[i].phone);
+
+void arrange(int num){
+    st *records = malloc(num * sizeof(st));
+     if (records == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(1);
     }
-}
 
-void arrange(int num,st *rec){
-    char temp[25];
-    // printf("This is arrange function\n");
+    //reading from file
+    FILE *myfile;
+    myfile=fopen("record.bin","rb");
+     if (myfile == NULL) {
+        printf("Error opening file for writing.\n");
+        exit(1);
+    }
+    fread(records,sizeof(st),num,myfile);
+    fclose(myfile);
+
+    // sorting the information alphabetically according to the name
+    st temp;
     for (int i=0;i<num;i++)
     {
         for (int j=i+1; j<num; j++)
         {
-            if (strcmp((rec+i)->name,(rec+j)->name)>0)
+            if (strcmp((records[i]).name,(records[j]).name)>0)
             {
-                strcpy(temp,(rec+i)->name);
-                strcpy((rec+i)->name,(rec+j)->name);
-                strcpy((rec+j)->name,temp);
+                temp = records[i];
+                records[i] = records[j];
+                records[j] = temp;
             }
         }
-    }	
+    }
+    printf("Sorted data alphabetically according to the name.\n");
+
+
+    // writing sorted data to the same file
+    myfile=fopen("record.bin","wb");
+    if (myfile == NULL) {
+        printf("Error opening file for writing.\n");
+        free(records);
+        exit(1);
+    }
+    fwrite(records, sizeof(st),  num, myfile);	
+    fclose(myfile);
+    free(records);
 }
 
-void display(int num,st *rec){
-    // printf("This is Output function\n");
-    printf("\nName\t\t\tRoll No.\t\tAddress\t\tPhone Number\n");
+void input(int num){
+    st rec;
+    FILE *myfile;
+    myfile=fopen("record.bin","wb");
+     if (myfile == NULL) {
+        printf("Error opening file for writing.\n");
+        exit(1);
+    }
     for (int i = 0; i < num; i++)
     {
-        printf("%s\t\t\t",rec[i].name);
-        printf("%d\t\t\t",rec[i].roll);
-        printf("%s\t\t\t",rec[i].address);
-        printf("%d\n",rec[i].phone);
+        printf("\nEnter the name of student:\t");
+        scanf("%s",rec.name);
+        printf("Enter the roll number:\t\t");
+        scanf("%d",&rec.roll);
+        printf("Enter the address:\t\t");
+        scanf("%s",rec.address);
+        printf("Enter the phone number:\t\t");
+        scanf("%lld",&rec.phone);
+        fwrite(&rec,sizeof(rec),1,myfile);
+    }
+    fclose(myfile);
+    printf("Record Added!!\n");
+}
+
+void displayRecord()
+{
+    st rec;
+    FILE *myfile;
+    int count=0;
+    myfile=fopen("record.bin","rb");
+     if (myfile == NULL) {
+        printf("Error opening file for writing.\n");
+        exit(1);
+    }
+    printf("\nName\t\t\tRoll No.\t\tAddress\t\t\tPhone Number\n");
+    while(fread(&rec,sizeof(rec),1,myfile)==1)
+    {
+        printf("%s\t\t\t%d\t\t\t%s\t\t\t%lld\n",rec.name,rec.roll,rec.address,rec.phone);
+        count++;
+    }
+    fclose(myfile);
+    if(count==0)
+    {
+        printf("No record Found");
+    }
+    else
+    {
+        printf("%d Records Found",count);
     }
 }
 
 
 int main() {
-    st rec[max];
     int num;
     printf("Enter the number of student:\t");
     scanf("%d",&num);
-    input(num,rec);
-    arrange(num,rec);
-    display(num,rec);
+    input(num);
+    arrange(num);
+    displayRecord();
     return 0;
 }
